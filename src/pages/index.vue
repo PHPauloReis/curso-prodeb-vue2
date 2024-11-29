ab5ee8e91a9bae9a2f97fcc097ba477b9e83e91c
Paulo Reis dos Santos <paulo.santos@prodeb.ba.gov.br>
2024-11-29 10:49:46 -0300

    commit inicial

---
+++ src/pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1>Calcule o seu IMC</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="12"
+      >
+        <v-text-field
+          v-model="peso"
+          label="Digite o seu peso"
+          required
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="12"
+      >
+        <v-text-field
+          v-model="altura"
+          label="Digite a sua altura"
+          required
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-btn
+          density="default"
+          color="indigo-darken-3"
+        >
+          Calcular
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      peso: 0,
+      altura: 0
+    }
+  }
+}
+
+</script>
